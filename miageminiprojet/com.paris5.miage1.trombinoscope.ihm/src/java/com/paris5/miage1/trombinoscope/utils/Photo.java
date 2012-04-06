3482f7fcd6e700808f653a9c6a964cb5a843940e
nedjahi.mourad <nedjahi.mourad@gmail.com>
2012-04-06 18:36:50 +0000

    photos

--- miageminiprojet/com.paris5.miage1.trombinoscope.ihm/src/java/com/paris5/miage1/trombinoscope/utils/Photo.java
+++ miageminiprojet/com.paris5.miage1.trombinoscope.ihm/src/java/com/paris5/miage1/trombinoscope/utils/Photo.java
@@ -11,19 +11,20 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 public class Photo {
-    
+
     BufferedImage img;
     private String image_url;
     private String image_ext;
     private String image_dest;
 
     public Photo(String image_url, String image_dest) {
+        super(ImageIO.read(new File(image_url)));
         this.image_url = image_url;
         String ext[] = image_url.split("\\.");
         this.image_ext = ext[0].toUpperCase();
         this.image_dest = image_dest;
     }
-    
+
     private BufferedImage resize(int targetWidth, int targetHeight, BufferedImage src) {
         double scaleW = (double) targetWidth / (double) src.getWidth();
         double scaleH = (double) targetHeight / (double) src.getHeight();
@@ -39,8 +40,13 @@ public class Photo {
 
         return result;
     }
-    
-    public void validate() throws IOException {
+
+    private BufferedImage cropImage(x, y, width, height) {
+        BufferedImage dest = img.getSubimage(x, y, width, height);
+        return dest;
+    }
+
+    public void flush() throws IOException {
         BufferedImage origin = ImageIO.read(new File(image_url));
         File dest = new File(image_dest);
         dest.createNewFile();
