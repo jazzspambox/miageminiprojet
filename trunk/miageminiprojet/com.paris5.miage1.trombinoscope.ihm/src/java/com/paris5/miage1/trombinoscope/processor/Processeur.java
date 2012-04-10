19a9abd3773245fd7b1188270b842fec5d9309e4
nedjahi.mourad@gmail.com <nedjahi.mourad@gmail.com@dba742a0-7f73-ec28-04a2-6286f2e03bf9>
2012-04-10 06:02:58 +0000

    ajout commentaires

--- trunk/miageminiprojet/com.paris5.miage1.trombinoscope.ihm/src/java/com/paris5/miage1/trombinoscope/processor/Processeur.java
+++ trunk/miageminiprojet/com.paris5.miage1.trombinoscope.ihm/src/java/com/paris5/miage1/trombinoscope/processor/Processeur.java
@@ -1,7 +1,22 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+/* ------------------------------------------------------------------------
+ 
+    Licensed under the Educational Community License version 1.0
+
+    This Original Work, including software, source code, documents,
+    or other related items, is being provided by the copyright holder(s)
+    subject to the terms of the Educational Community License. By
+    obtaining, using and/or copying this Original Work, you agree that you
+    have read, understand, and will comply with the following terms and
+    conditions of the Educational Community License:
+
+    Permission to use, copy, modify, merge, publish, distribute, and
+    sublicense this Original Work and its documentation, with or without
+    modification, for any purpose, and without fee or royalty to the
+    copyright holder(s) is hereby granted, provided that you include the
+    following on ALL copies of the Original Work or portions thereof,
+    including modifications or derivatives.
+
+ ------------------------------------------------------------------------ */
 package com.paris5.miage1.trombinoscope.processor;
 
 import java.io.IOException;
@@ -9,12 +24,13 @@ import java.sql.SQLException;
 import javax.servlet.ServletException;
 
 /**
- *
- * @author mourad
+ * interface permettant de definir la methode de declenchement d un processus au saint de l application
+ * @author Mourad, Saliou, Idir
  */
 public interface Processeur {
+    
     /**
-     * 
+     * constructeur
      * @throws SQLException
      * @throws ServletException
      * @throws IOException 
--- trunk/miageminiprojet/com.paris5.miage1.trombinoscope.ihm/src/java/com/paris5/miage1/trombinoscope/processor/Zone.java
+++ trunk/miageminiprojet/com.paris5.miage1.trombinoscope.ihm/src/java/com/paris5/miage1/trombinoscope/processor/Zone.java
@@ -1,3 +1,22 @@
+/* ------------------------------------------------------------------------
+ 
+    Licensed under the Educational Community License version 1.0
+
+    This Original Work, including software, source code, documents,
+    or other related items, is being provided by the copyright holder(s)
+    subject to the terms of the Educational Community License. By
+    obtaining, using and/or copying this Original Work, you agree that you
+    have read, understand, and will comply with the following terms and
+    conditions of the Educational Community License:
+
+    Permission to use, copy, modify, merge, publish, distribute, and
+    sublicense this Original Work and its documentation, with or without
+    modification, for any purpose, and without fee or royalty to the
+    copyright holder(s) is hereby granted, provided that you include the
+    following on ALL copies of the Original Work or portions thereof,
+    including modifications or derivatives.
+
+ ------------------------------------------------------------------------ */
 package com.paris5.miage1.trombinoscope.processor;
 
 import com.paris5.miage1.trombinoscope.utils.Configuration;
